40cb24f86f4d57383e6332ee14c15b7697c91c28
dreialcantara <andreialcantara@id.uff.br>
2022-10-26 15:03:03 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,237 @@
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+import TestHtml from '../src/components/TestHtml'
+
+export default function App() {
+  const form = useRef();
+
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const [fromEmail, setFromEmail] = useState("");
+  const [fromName, setFromName] = useState("");
+  const cardResultado = "https://i.ibb.co/YhC5yFF/C-pia-de-WOW-Cards-Feedbacks-Atendimento.png"
+
+  const bgimage = require("../src/img/wowcardatendimento.png")
+  const sendParams = {
+    to_email: email,
+    to_name: name,
+    message: message,
+    fromName: fromName,
+    from_email: fromEmail,
+    html: `<head>
+    <title></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--[if mso]><xml><o:OfficeDocumentSettings><o:PixelsPerInch>96</o:PixelsPerInch><o:AllowPNG/></o:OfficeDocumentSettings></xml><![endif]-->
+    <style>
+      * {
+        box-sizing: border-box;
+      }
+  
+      body {
+        margin: 0;
+        padding: 0;
+      }
+  
+      a[x-apple-data-detectors] {
+        color: inherit !important;
+        text-decoration: inherit !important;
+      }
+  
+      #MessageViewBody a {
+        color: inherit;
+        text-decoration: none;
+      }
+  
+      p {
+        line-height: inherit
+      }
+  
+      .desktop_hide,
+      .desktop_hide table {
+        mso-hide: all;
+        display: none;
+        max-height: 0px;
+        overflow: hidden;
+      }
+  
+      @media (max-width:520px) {
+        .desktop_hide table.icons-inner {
+          display: inline-block !important;
+        }
+  
+        .icons-inner {
+          text-align: center;
+        }
+  
+        .icons-inner td {
+          margin: 0 auto;
+        }
+  
+        .row-content {
+          width: 100% !important;
+        }
+  
+        .mobile_hide {
+          display: none;
+        }
+  
+        .stack .column {
+          width: 100%;
+          display: block;
+        }
+  
+        .mobile_hide {
+          min-height: 0;
+          max-height: 0;
+          max-width: 0;
+          overflow: hidden;
+          font-size: 0px;
+        }
+  
+        .desktop_hide,
+        .desktop_hide table {
+          display: table !important;
+          max-height: none !important;
+        }
+      }
+    </style>
+  </head>
+  
+  <body style="background-color: #FFFFFF; margin: 0; padding: 0; -webkit-text-size-adjust: none; text-size-adjust: none;">
+    <table class="nl-container" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #FFFFFF; background-image: url(${cardResultado}); background-position: top center; background-size: auto; background-repeat: no-repeat;">
+      <tbody>
+        <tr>
+          <td>
+            <table class="row row-1" align="center" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+              <tbody>
+                <tr>
+                  <td>
+                    <table class="row-content stack" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-radius: 0; color: #000000; width: 500px;" width="500">
+                      <tbody>
+                        <tr>
+                          <td class="column column-1" width="100%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-top: 5px; padding-bottom: 5px; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;">
+                            <table class="heading_block block-2" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+                              <tr>
+                                <td class="pad" style="text-align:center;width:100%;padding-top:60px;">
+                                  <h1 style="margin: 0; color: #000000; direction: ltr; font-family: Arial, Helvetica Neue, Helvetica, sans-serif; font-size: 30px; font-weight: 700; letter-spacing: normal; line-height: 120%; text-align: center; margin-top: 0; margin-bottom: 0;"><span class="tinyMce-placeholder">Olá ${name}!</span></h1>
+                                </td>
+                              </tr>
+                            </table>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <table class="row row-2" align="center" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+              <tbody>
+                <tr>
+                  <td>
+                    <table class="row-content stack" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-radius: 0; color: #000000; width: 500px;" width="500">
+                      <tbody>
+                        <tr>
+                          <td class="column column-1" width="100%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-top: 5px; padding-bottom: 5px; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;">
+                            <table class="paragraph_block block-1" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;">
+                              <tr>
+                                <td class="pad" style="padding-bottom:610px;padding-left:10px;padding-right:10px;padding-top:10px;">
+                                  <div style="color:#000000;direction:ltr;font-family:Arial, Helvetica Neue, Helvetica, sans-serif;font-size:20px;font-weight:700;letter-spacing:0px;line-height:120%;text-align:left;mso-line-height-alt:16.8px;">
+                                    <p style="margin: 0;">${message}</p>
+                                  </div>
+                                </td>
+                              </tr>
+                            </table>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <table class="row row-3" align="center" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+              <tbody>
+                <tr>
+                  <td>
+                    <table class="row-content stack" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000000; width: 500px;" width="500">
+                      <tbody>
+                        <tr>
+                          <td class="column column-1" width="100%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-top: 5px; padding-bottom: 5px; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;">
+                            <table class="icons_block block-1" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+                              <tr>
+                                <td class="pad" style="vertical-align: middle; color: #9d9d9d; font-family: inherit; font-size: 15px; padding-bottom: 5px; padding-top: 5px; text-align: center;">
+                                  <table width="100%" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+                                    <tr>
+                                      <td class="alignment" style="vertical-align: middle; text-align: center;">
+                                        <!--[if vml]><table align="left" cellpadding="0" cellspacing="0" role="presentation" style="display:inline-block;padding-left:0px;padding-right:0px;mso-table-lspace: 0pt;mso-table-rspace: 0pt;"><![endif]-->
+                                        <!--[if !vml]><!-->
+                                        <table class="icons-inner" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; display: inline-block; margin-right: -4px; padding-left: 0px; padding-right: 0px;" cellpadding="0" cellspacing="0" role="presentation">
+                                          <!--<![endif]-->
+                                          <tr>
+                                            <td style="vertical-align: middle; text-align: center; padding-top: 5px; padding-bottom: 5px; padding-left: 5px; padding-right: 6px;"><a href="https://www.designedwithbee.com/" target="_blank" style="text-decoration: none;"><img class="icon" alt="Designed with BEE" src="https://d15k2d11r6t6rl.cloudfront.net/public/users/Integrators/BeeProAgency/53601_510656/Signature/bee.png" height="32" width="34" align="center" style="display: block; height: auto; margin: 0 auto; border: 0;"></a></td>
+                                            <td style="font-family: Arial, Helvetica Neue, Helvetica, sans-serif; font-size: 15px; color: #9d9d9d; vertical-align: middle; letter-spacing: undefined; text-align: center;"><a href="https://www.designedwithbee.com/" target="_blank" style="color: #9d9d9d; text-decoration: none;">Designed with BEE</a></td>
+                                          </tr>
+                                        </table>
+                                      </td>
+                                    </tr>
+                                  </table>
+                                </td>
+                              </tr>
+                            </table>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </td>
+        </tr>
+      </tbody>
+    </table><!-- End -->
+  </body>`
+
+  }
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.send('service_26q3uhg', 'template_i0zxgbl', sendParams, 'al70E8s8MFY9LLhTe')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
+
+  return (
+
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input value={name}
+        onChange={(event) => {
+          setName(event.target.value);
+        }} type="text" />
+      <label>Email</label>
+      <input value={email}
+        onChange={(event) => {
+          setEmail(event.target.value);
+        }} type="email" />
+      <label>Email Remetente</label>
+      <input value={fromEmail}
+        onChange={(event) => {
+          setFromEmail(event.target.value);
+        }} type="email" />
+      <label>Message</label>
+      <textarea value={message}
+        onChange={(event) => {
+          setMessage(event.target.value);
+        }} name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
---
+++ src/components/TestHtml/index.jsx
@@ -0,0 +1,11 @@
+import "./index.css"
+
+export default function testehtml(){
+
+    return (
+        <div className="background">
+            
+        </div>
+    )
+
+}
